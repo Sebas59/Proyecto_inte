9c2d18f4338337a7c06c41d8ff6da5f4240743d5
Sebastian lopez <juancholour@gmail.com>
2025-05-09 21:32:33 -0500

    Se corrigio modelo vehiculo create

--- data/schemas.py
+++ data/schemas.py
@@ -1,13 +1,12 @@
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel,Form,Field
 from data.models import *
 from typing import Optional
 
 class VehiculoCreate(SQLModel):
-    marca: str
-    modelo: str
-    year: int
-    Tipo_combustible: Tipo_combustibleEnum
-    Tan_size: float
+    marca: str = Form(...), 
+    modelo: Optional[str] = Form(...),
+    Tipo_combustible: Optional[Tipo_combustibleEnum] = Form(None),
+    Tan_size: Optional[float] = Form(default=0.0),
 
 class VehiculoRead(SQLModel):
     id: Optional[int] = None
