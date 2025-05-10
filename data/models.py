656e8ea8b435d16f8aac92d37a7666212826cb92
Sebastian lopez <juancholour@gmail.com>
2025-05-09 20:10:17 -0500

    Se agrego modelo enum para tipo combustible

--- data/models.py
+++ data/models.py
@@ -5,10 +5,15 @@ from datetime import datetime
 import enum
 
 class Vehiculo(SQLModel, table=True):
+    __tablename__ = "Vehiculo"
     id: Optional[int] = Field(default=None, primary_key=True)
     marca: str
     modelo: str
     year: int
     Tipo_combustible: str
     Tan_size: float
+
+class Tipo_combustibleEnum(str, enum.Enum):
+    gasolina = "Gasolina"
+    diesel = "Diesel"
     
