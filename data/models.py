8c43d53cb16c663498bd4852c4bc553df1d18eea
Sebastian lopez <juancholour@gmail.com>
2025-05-09 20:12:22 -0500

    Se agrego asignacion del modelo enum

--- data/models.py
+++ data/models.py
@@ -4,16 +4,16 @@ from sqlalchemy import Enum as SqlEnum
 from datetime import datetime
 import enum
 
+class Tipo_combustibleEnum(str, enum.Enum):
+    gasolina = "Gasolina"
+    diesel = "Diesel"
+    
+
 class Vehiculo(SQLModel, table=True):
     __tablename__ = "Vehiculo"
     id: Optional[int] = Field(default=None, primary_key=True)
     marca: str
     modelo: str
     year: int
-    Tipo_combustible: str
+    Tipo_combustible: Tipo_combustibleEnum
     Tan_size: float
-
-class Tipo_combustibleEnum(str, enum.Enum):
-    gasolina = "Gasolina"
-    diesel = "Diesel"
-    
