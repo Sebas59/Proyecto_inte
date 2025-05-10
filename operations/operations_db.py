ce691063d2d43c45411471308a5b231d2c73ad1b
Sebastian lopez <juancholour@gmail.com>
2025-05-09 21:53:35 -0500

    Se agrego funcion para aceptar datos form

--- operations/operations_db.py
+++ operations/operations_db.py
@@ -3,9 +3,25 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.future import select
 from sqlalchemy import and_
 from fastapi import HTTPException, status
-from typing import List
-from data.models import Vehiculo
+from typing import List, Optional
+from data.models import Vehiculo, Tipo_combustibleEnum
 from data.schemas import VehiculoCreate
+from fastapi import Form
+
+def vehiculo_create_form(
+    marca: str = Form(...),
+    modelo: Optional[str] = Form(...),
+    Tipo_combustible: Optional[Tipo_combustibleEnum] = Form(None),
+    Tan_size: Optional[float] = Form(0.0),
+    year: Optional[int] = Form(...)
+) -> VehiculoCreate:
+    return VehiculoCreate(
+        marca=marca,
+        modelo=modelo,
+        Tipo_combustible=Tipo_combustible,
+        Tan_size=Tan_size,
+        year=year
+    )
 
 async def crear_vehiculo_db(vehiculo:VehiculoCreate, session:AsyncSession)->Vehiculo:
     nuevo_vehiculo = Vehiculo(**vehiculo.dict())
