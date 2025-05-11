8b9234933d5d382524f88886d0195ed836e2288a
Sebastian lopez <juancholour@gmail.com>
2025-05-10 20:48:01 -0500

    Se corrigio

--- data/schemas.py
+++ data/schemas.py
@@ -4,11 +4,11 @@ from typing import Optional
 from fastapi import Form
 
 class VehiculoCreate(SQLModel):
-    marca: str = Form(...)
-    modelo: Optional[str] = Form(...)
-    year: int = Form(...)
-    Tipo_combustible: Optional[Tipo_combustibleEnum] = Form(None)
-    Tan_size: Optional[float] = Form(default=0.0)
+    marca: str
+    modelo: str
+    year: int
+    Tipo_combustible: Tipo_combustibleEnum
+    Tan_size: float
 
 class VehiculoRead(SQLModel):
     id: Optional[int] = None
