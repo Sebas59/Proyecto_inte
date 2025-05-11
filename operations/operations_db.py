162a715d1a0809ea2bd299e30dd5442557ac0ae0
Sebastian lopez <juancholour@gmail.com>
2025-05-11 16:28:16 -0500

    se agrego funcion para actualizar precio de combustible

--- operations/operations_db.py
+++ operations/operations_db.py
@@ -106,3 +106,18 @@ async def obtener_precio_combustible_db(session:AsyncSession)->List[CombustibleR
     result = await session.execute(select(Combustible))
     precio_combustible = result.scalars().all()
     return precio_combustible
+
+async def actualizar_precio_combustible_db(id:int, combustible:CombustibleCreate, session:AsyncSession)->Combustible:
+    nuevo_precio = await session.get(Combustible, id)
+    if not nuevo_precio:
+        raise HTTPException(status_code=404, detail="Precio de combustible no encontrado")
+    for key, value in combustible.dict(exclude_unset=True).items():
+        setattr(nuevo_precio, key, value)
+    session.add(nuevo_precio)
+    try:
+        await session.commit()
+        await session.refresh(nuevo_precio)
+        return nuevo_precio
+    except IntegrityError:
+        await session.rollback()
+        raise HTTPException(status_code=404, detail="Error al actualizar el precio del combustible")
