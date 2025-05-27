1770c6aec1508dbfc9e4ff701837f5f0b2cc4f3f
Sebastian lopez <juancholour@gmail.com>
2025-05-27 18:46:20 -0500

    se agrego router de / en home

---
+++ home.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Depends, HTTPException, status, Form
+from fastapi.requests import Request
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import List, Optional
+from contextlib import asynccontextmanager
+from fastapi.responses import HTMLResponse
+
+
+from data.models import *
+from utils.connection_db import *
+from data.schemas import *
+from operations.operations_db import ( 
+    obtener_vehiculos_db,
+    crear_vehiculo_db,
+    actualizar_vehiculo_db,
+    eliminar_vehiculo_db,
+    obtener_vehiculo_por_marca_modelo_db,
+    vehiculo_create_form, 
+    combustible_create_form, 
+    crear_combustible_precio_db, 
+    obtener_precio_combustible_db, 
+    actualizar_precio_combustible_db, 
+    eliminar_precio_combustible_db, 
+    obtener_vehiculos_con_costo_combustible_db 
+)
+
+
+
+@asynccontextmanager
+async def lifespan(app: APIRouter):
+    await init_db()
+    yield
+
+templades = Jinja2Templates(directory="templates")
+router = APIRouter(lifespan=lifespan)
+
+@router.get("/", response_class=HTMLResponse)
+async def leer_home(request: Request):
+    return templades.TemplateResponse("home.html", {"request": request})
+
